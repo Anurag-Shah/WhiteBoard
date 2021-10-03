a2f6f125bf9868f2001ef770160765a3e1714a7d
Anurag-Shah <anuragshah2000@gmail.com>
2021-10-03 16:04:08 -0400

    setup wrappers for ocr, compiler

---
+++ Backend/Compiler/compiler_wrapper.py
@@ -0,0 +1,19 @@
+
+#############################################################################
+# compiler_wrapper.py
+#
+# Authors: Anurag
+#
+# Provides a wrapper interface for the compiler process
+# This includes programming language detection
+#############################################################################
+
+# ocr_wrapper
+# Author: Anurag
+# Return value: string of data
+# Parameters:
+#	1. text - text to compile
+# This function is a wrapper for the entire Compiler Process
+
+compiler_wrapper(text):
+	return ""
---
+++ Backend/OCR/ocr_wrapper.py
@@ -0,0 +1,20 @@
+
+#############################################################################
+# ocr_wrapper.py
+#
+# Authors: Anurag
+#
+# Provides a wrapper interface to the rest of the OCR code
+# It takes in an image and any other related data we decide to add later
+# Its output is the text read from the image
+#############################################################################
+
+# ocr_wrapper
+# Author: Anurag
+# Return value: string of data
+# Parameters:
+#	1. image - image matrix
+# This function is a wrapper for the entire OCR Process
+
+ocr_wrapper(image):
+	return ""
