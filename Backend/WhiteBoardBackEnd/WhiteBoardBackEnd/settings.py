ac740c64574e030bf0c8075392fa0f50258f475b
Chunao Liu <787811283@qq.com>
2021-10-03 15:17:54 -0400

    VirtualEnv Setup + MySQL setup

--- Backend/WhiteBoardBackEnd/WhiteBoardBackEnd/settings.py
+++ Backend/WhiteBoardBackEnd/WhiteBoardBackEnd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'API',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'WhiteBoardBackEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_data_base',
+        'USER': 'root',
+        'PASSWORD': '990603qwerty',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
