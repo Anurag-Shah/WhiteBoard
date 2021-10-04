77b636b34dfd1a535c93b612ac254531747a73a8
Anurag-Shah <anuragshah2000@gmail.com>
2021-10-03 21:12:18 -0400

    working tesseract

--- Backend/OCR/ocr_wrapper.py
+++ Backend/OCR/ocr_wrapper.py
@@ -19,10 +19,13 @@ import ocr_handwritten
 import ocr_typeform
 import ocr_postprocess_text
 import ocr_texttype_detection
+import ocr_lang_detect
+
+from PIL import Image
 
 # ocr_wrapper
 # Author: Anurag
-# Return value: string of data
+# Return value: (string of data, programming langauge)
 # Parameters:
 #	1. image - image matrix
 # This function is a wrapper for the entire OCR Process
@@ -36,26 +39,34 @@ import ocr_texttype_detection
 #	ocr_postprocess_text.py
 #	ocr_texttype_detection.py
 
-def ocr_wrapper(image):
+def ocr_wrapper(image):	
 	texttype = ocr_texttype_detection.detect(image)
+	code = ""
 	if texttype == "typeform":
 		im = preprocess_typeform.preprocess_image(image)
 		out = ""
 		for char in charseg_typeform.segment(im):
 			out += ocr_typeform.ocr_typeform(im)
-		return ocr_postprocess_text.ocr_postprocess(out)
+		code = ocr_postprocess_text.ocr_postprocess(out)
 	elif texttype == "handwritten":
 		im = preprocess_handwritten.preprocess_image(image)
 		out = ""
 		for char in charseg_handwritten.segment(im):
 			out += ocr_handwritten.ocr_handwritten(im)
-		return ocr_postprocess_text.ocr_postprocess(out)
+		code = ocr_postprocess_text.ocr_postprocess(out)
 	elif texttype == "typeform_pretrained":
 		# This will be removed when obsolete
-		return ocr_typeform.ocr_tesseract(image)
+		out = ocr_typeform.ocr_tesseract(image)
+		code = ocr_postprocess_text.tesseract_postprocess(out)
 	else:
 		raise OCRError
+	language = ocr_lang_detect.detect(code)
+	return (code, language)
+
 
 if __name__ == "__main__":
 	# Testing function for pipeline
-	print(ocr_wrapper("test"))
+	test_im_path = "images/tesseract_tests/"
+	test_im = "courierb_3"
+	imsuffix = ".png"
+	print(ocr_wrapper(Image.open(test_im_path + test_im + imsuffix)))
