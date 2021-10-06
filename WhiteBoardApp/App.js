cb8d160c58e6d62dad69f7c11caaab91bc246609
erpan42 <81669276+erpan42@users.noreply.github.com>
2021-10-06 14:14:45 -0400

    expo env

---
+++ WhiteBoardApp/App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+export default function App() {
+  return <WelcomeScreen />;
+}
