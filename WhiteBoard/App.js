33f6cf91d0e5182382d38985d421bc84dd3f26a5
erpan42 <81669276+erpan42@users.noreply.github.com>
2021-10-06 14:16:12 -0400

    Old deleted

--- WhiteBoard/App.js
+++
@@ -1,8 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-
-export default function App() {
-  return <WelcomeScreen />;
-}
