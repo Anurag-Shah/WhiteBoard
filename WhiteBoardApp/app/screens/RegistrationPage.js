a05a2e7bd3ad410dbbdc0ed2bfa90a46f72fbb5f
Michelle He <he517@purdue.edu>
2021-10-10 18:45:44 -0400

    registration page UI complete

--- WhiteBoardApp/app/screens/RegistrationPage.js
+++ WhiteBoardApp/app/screens/RegistrationPage.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Image, TextInput, View, Button } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 
 function RegistrationPage(props) {
+    validateEmail = (email) => {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Image
-                style={styles.image}
-                source={require('../assets/logo.png')}
+        <SafeAreaView>
+            <Icon
+                style={styles.icon}
+                name='arrow-undo-outline'
+                type='ionicon'
+                color='#000'
+                alignItems='left'
+                //onPress
             />
-            <TextInput
-                style={styles.input}
-                placeholder="username"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="email address"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="password"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="confirm password"
-            />
-            <View style={styles.button}><Button title="Sign up" color="#fff"/></View>
+            <View style={styles.container}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/logo.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="username"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="email address"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="password"
+                    contextMenuHidden={true}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="confirm password"
+                    contextMenuHidden={true}
+                />
+                <View style={styles.button}>
+                    <Button 
+                        title="Sign up" 
+                        color="#fff"
+                        //onPress
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    icon: {
+        marginLeft: 30,
+    },
+    
     container: {
-        flex: 1,
+        //flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
@@ -41,7 +69,8 @@ const styles = StyleSheet.create({
     image: {
         width: 150,
         height: 150,
-        marginBottom: 20,
+        marginTop: 50,
+        marginBottom: 30,
     },
 
     input: {
