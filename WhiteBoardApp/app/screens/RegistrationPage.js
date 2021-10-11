5a2db6177ff324b8efdef92559c1e25019b9d72e
Michelle He <he517@purdue.edu>
2021-10-10 20:19:32 -0400

    saving field values and additional features

--- WhiteBoardApp/app/screens/RegistrationPage.js
+++ WhiteBoardApp/app/screens/RegistrationPage.js
@@ -1,92 +1,113 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Image, TextInput, View, Button } from 'react-native';
-import { Icon } from 'react-native-elements'
-
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TextInput,
+  Text,
+  View,
+  Button,
+} from "react-native";
+import { Icon } from "react-native-elements";
 
 function RegistrationPage(props) {
-    validateEmail = (email) => {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    };
+  const [username, onChangeUsername] = React.useState(null);
+  const [email, onChangeEmail] = React.useState(null);
+  const [password, onChangePassword] = React.useState(null);
+  const [confirm, onChangeConfirm] = React.useState(null);
 
-    return (
-        <SafeAreaView>
-            <Icon
-                style={styles.icon}
-                name='arrow-undo-outline'
-                type='ionicon'
-                color='#000'
-                alignItems='left'
-                //onPress
-            />
-            <View style={styles.container}>
-                <Image
-                    style={styles.image}
-                    source={require('../assets/logo.png')}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="username"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="email address"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="password"
-                    contextMenuHidden={true}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="confirm password"
-                    contextMenuHidden={true}
-                />
-                <View style={styles.button}>
-                    <Button 
-                        title="Sign up" 
-                        color="#fff"
-                        //onPress
-                    />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView>
+      <Icon
+        style={styles.icon}
+        name="arrow-undo-outline"
+        type="ionicon"
+        color="#000"
+        alignItems="left"
+        //onPress
+      />
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("../assets/logo.png")} />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeUsername}
+          value={username}
+          placeholder="username"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="email address"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="password"
+          contextMenuHidden={true}
+          secureTextEntry={true}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeConfirm}
+          value={confirm}
+          placeholder="confirm password"
+          contextMenuHidden={true}
+          secureTextEntry={true}
+          onEndEditing={() => {
+            if (password == confirm) {
+              console.log("same");
+            } else {
+              console.log("not same");
+            }
+          }}
+        />
+        <View style={styles.button}>
+          <Button
+            title="Sign up"
+            color="#fff"
+            onPress={() => console.log("Sign up success!")} //TODO
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    icon: {
-        marginLeft: 30,
-    },
-    
-    container: {
-        //flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  icon: {
+    marginLeft: 30,
+  },
 
-    image: {
-        width: 150,
-        height: 150,
-        marginTop: 50,
-        marginBottom: 30,
-    },
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    input: {
-        width: 300,
-        height: 45,
-        margin: 10,
-        borderWidth: 1,
-        padding: 10,
-    },
+  image: {
+    width: 150,
+    height: 150,
+    marginTop: 50,
+    marginBottom: 30,
+  },
 
-    button: {
-        width: 110,
-        height: 40,
-        backgroundColor: "green",
-        margin: 30,
-    }
+  input: {
+    width: 300,
+    height: 45,
+    margin: 10,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  button: {
+    width: 110,
+    height: 40,
+    backgroundColor: "green",
+    margin: 30,
+  },
 });
 
 export default RegistrationPage;
