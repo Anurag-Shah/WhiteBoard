5b1dee7080f56f2bb8d592363fa0e162467f485a
janneynana <JANNEYNANANA@GMAIL.COM>
2021-10-13 17:04:20 -0400

    update4

--- WhiteBoardApp/App.js
+++ WhiteBoardApp/App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import WelcomeScreen from './app/screens/LoginPage';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginPage from './app/screens/LoginPage';
-import CameraPage from './app/screens/CameraPage';
-
-
-const Stack = createNativeStackNavigator();
+import React from "react";
+import RegistrationPage from "./app/screens/RegistrationPage";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="CameraPage">
-        <Stack.Screen name="Camera" component={CameraPage} />
-        <Stack.Screen name="Login" component={LoginPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <RegistrationPage />;
 }
