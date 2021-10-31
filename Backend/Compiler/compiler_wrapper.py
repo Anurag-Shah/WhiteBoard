f1bcefa598cd34550c52305bc3757bcd201c06d3
Anurag-Shah <anuragshah2000@gmail.com>
2021-10-31 01:51:37 -0400

    Compiler tracks line numbers

--- Backend/Compiler/compiler_wrapper.py
+++ Backend/Compiler/compiler_wrapper.py
@@ -30,11 +30,19 @@ def compiler_wrapper(compiler_input, language):
 		os.remove("compile.c")
 		if (os.path.exists("a.out")):
 			os.remove("a.out")
-		return proc.stdout, line_numbers
+		out_text = str(proc.stdout)
+		out_text = out_text.replace("./compile_c.sh: 2: ./a.out: not found\n", "")
+		matches = re.findall("compile\.c:[0-9]+:[0-9]+:", out_text)
+		for match in matches:
+			temp_m = match.replace("compile.c:", "")
+			temp_m = temp_m.split(':')
+			if int(temp_m[0]) not in line_numbers:
+				line_numbers += [int(temp_m[0])]
+		return out_text, line_numbers
 	else:
 		"Language Not Supported"
 	return None, None
 
 if __name__ == "__main__":
 	# Testing function for pipeline
-	compiler_wrapper(('#include<stdio.h>\n\nint main(int argc, char *argv[]) {\nprintf ("Hello World") ;\nreturn 0;\n\n}', "C"))
+	print(compiler_wrapper('#include<stdio.h>\n\nint main(int argc, char *argv[]) {\nprintf ("Hello World") ;\nreturn 0;\n\n}', "C"))
