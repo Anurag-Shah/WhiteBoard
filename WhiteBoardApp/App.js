03197d2c4543cfc2fc4945d6597cf4e8112a3cd8
janneynana <JANNEYNANANA@GMAIL.COM>
2021-10-31 20:33:22 -0400

    solve frontend conflicts

--- WhiteBoardApp/App.js
+++ WhiteBoardApp/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Navigation from "./app/Navigation";
+import RegistrationPage from "./app/screens/RegistrationPage";
+// import TextEditorPage from "./app/screens/TextEditorPage.js";
+
 
 export default function App() {
-  return <Navigation />;
+  return <WelcomeScreen />;
 }
