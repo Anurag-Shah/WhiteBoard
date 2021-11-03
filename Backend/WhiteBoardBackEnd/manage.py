f9fcf1c0e58bf6b27c837a8c7c95befba159fbf8
Anurag-Shah <shah447@purdue.edu>
2021-11-03 00:20:46 -0400

    Prune containers every 10 mins

--- Backend/WhiteBoardBackEnd/manage.py
+++ Backend/WhiteBoardBackEnd/manage.py
@@ -2,14 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import threading
 
 def main():
     """Run administrative tasks."""
     sys.path.insert(1, os.path.abspath("API"))
     sys.path.insert(1, os.path.abspath("../Compiler"))
     sys.path.insert(1, os.path.abspath("../OCR"))
-    print(sys.path)
+    import prune_containers     # Cant move this import outside because of path
+                                # If you want, can move that file from ../Compiler to this dir
+                                # Really doesn't matter, python doesnt care where you import
+                                # Its just convention to do it at the top of the file
+                                # Or important if there are multiple functions
+    threading.Thread(target=prune_containers.prune, args=(600,)).start()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhiteBoardBackEnd.settings')
     try:
         from django.core.management import execute_from_command_line
