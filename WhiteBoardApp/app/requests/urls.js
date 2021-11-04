5794f34fa8ecd100641fde423ac2f6b76c9ce8e4
janneynana <66856643+janneynana@users.noreply.github.com>
2021-11-04 15:42:25 -0400

    Update urls.js

--- WhiteBoardApp/app/requests/urls.js
+++ WhiteBoardApp/app/requests/urls.js
@@ -5,7 +5,7 @@ export default {
     logout: base_url + "User/logout/",
     resetPwd: base_url + "password_reset",
     updateAccount: base_url + "User/update/",
-    avatar: base_url + "User/avatar",
+    avatar: base_url + "User/avatar/",
     group_operations: base_url + "User/group/",
-    memeber_operations: base_url + "User/group/memeber",
+    memeber_operations: base_url + "User/group/memeber/",
 }
