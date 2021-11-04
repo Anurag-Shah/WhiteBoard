03f2522f07ff19730a81a97e857c44952a77390f
Michelle He <54548181+he517@users.noreply.github.com>
2021-11-04 17:40:58 -0400

    Update urls.js

--- WhiteBoardApp/app/requests/urls.js
+++ WhiteBoardApp/app/requests/urls.js
@@ -1,4 +1,4 @@
-const base_url = "http://ec2-3-138-112-15.us-east-2.compute.amazonaws.com:8080/"
+const base_url = "http://ec2-3-138-112-15.us-east-2.compute.amazonaws.com:8000/"
 
 export default {
     login: base_url + "User/login/",
