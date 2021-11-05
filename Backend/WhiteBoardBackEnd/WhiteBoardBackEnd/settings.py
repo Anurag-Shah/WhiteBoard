d3e2392191e71ad9a6aa44948f546e58cfda4bc8
jackey6621 <jackey.yjy@gmail.com>
2021-11-04 20:41:55 -0400

    removed

--- Backend/WhiteBoardBackEnd/WhiteBoardBackEnd/settings.py
+++ Backend/WhiteBoardBackEnd/WhiteBoardBackEnd/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qk!d=*&ds!@-jw8%8fu7qpx1xcsy5-0qnkt394+$k+^ua2mlil'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
