9a0f875bcbbbe2abdf7230731ed5a0c863eb4acf
janneynana <JANNEYNANANA@GMAIL.COM>
2021-11-29 17:34:38 -0500

    backend update

--- Backend/WhiteBoardBackEnd/API/urls.py
+++ Backend/WhiteBoardBackEnd/API/urls.py
@@ -1,24 +1,34 @@
 from django.urls import path
 
 from .views import *
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('Users/', AllUserList.as_view()),
     path('Users/<int:id>', SpecificUser.as_view()),
     path('User/login/', login_view, name='login'),
-    path('User/register/', sign_up, name='register'),
     path('User/logout/', logout_view, name='logout'),
     path('User/update/', update_user, name='account_update'),
     path('User/avatar/', Avatar.as_view(), name='get_or_set_avatar'),
+    path('User/groups/', UserGroups.as_view(), name='get_user_groups'),
+    path('User/groups/<int:uid>', UserGroups.as_view(), name='get_user_groups'),
+    path('User/group/', GroupOperations.as_view(),
+         name='create_or_delete_a_team'),
+    path('User/group/members/', allMembers, name='get_members'),
+    path('User/group/member/', GroupMemberOperations.as_view(),
+         name='add_or_delete_member'),
     path('Group/<int:id>', SpecificGroup.as_view()),
+    path('TextUpload/<int:id>', TextUpload.as_view()),
+    path('TempTextUpload/', TempTextUpload.as_view()),
+    path('TempImages/', TempImageUpload.as_view()),
+    path('ImageDeleteByID/<int:id>', ImageDeleteWithID.as_view()),
     path('Images/<int:GPid>', ImageUpload.as_view()),
     path('Images/process', process_image, name='process_image'),
-    path('Text/process', process_image, name='process_image'),
+    path('Text/process', process_text, name='process_image'),
     path("password_reset", pwd_reset, name="password_reset"),
     path('password_reset_confirm/<uidb64>/<token>',
          PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('password_reset/done/', password_reset_done, name='password_reset_done'),
 
+    path('Register/', register),
 ]
