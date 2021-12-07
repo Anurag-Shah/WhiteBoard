20ba83269ded8fa517dd8f15d13bec312c0102d1
Anurag-Shah <shah447@purdue.edu>
2021-12-07 17:29:30 -0500

    tests updated

--- Backend/WhiteBoardBackEnd/API/tests.py
+++ Backend/WhiteBoardBackEnd/API/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from compiler_wrapper import compiler_wrapper
 from ocr_lang_detect import detect
 
-'''class CompilerTestCase(TestCase):
+class CompilerTestCase(TestCase):
 	def setUp(self):
 		self.c_execute = '#include<stdio.h>\n\nint main(int argc, char *argv[]) {\nprintf ("Hello World") ;\nreturn 0;\n\n}'
 		self.c_error = '#incelude<stdio.h>\n\nint main(int argc, char *argv[]) {\nprintf ("Hello World") ;\nreturn 0;\n\n}'
@@ -20,34 +20,42 @@ from ocr_lang_detect import detect
 		lang = detect(self.c_execute)
 		out1 = compiler_wrapper(self.c_execute, lang)
 		out2 = compiler_wrapper(self.c_error, lang)
+		self.assertEqual(lang, "C")
+		while out1[0] == '':
+			out1 = compiler_wrapper(self.c_execute, lang)
+		while out2[0] == '':
+			out2 = compiler_wrapper(self.c_error, lang)
 		self.assertEqual(out1[0], "Hello World")
 		self.assertEqual(out1[1], [])
 		self.assertNotEqual(out2[0], "Hello World")
-		self.assertEqual(lang, "C")
 
 	def test_cpp(self):
 		lang = detect(self.cpp_execute) 
 		out1 = compiler_wrapper(self.cpp_execute, lang)
 		out2 = compiler_wrapper(self.cpp_error, lang)
+		self.assertEqual(lang, "C++")
+		while out1[0] == '':
+			out1 = compiler_wrapper(self.cpp_execute, lang)
+		while out2[0] == '':
+			out2 = compiler_wrapper(self.cpp_error, lang)
 		self.assertEqual(out1[0], "Hello World")
 		self.assertEqual(out1[1], [])
 		self.assertNotEqual(out2[0], "Hello World")
-		self.assertEqual(lang, "C++")
 
 	def test_cs(self):
 		lang = detect(self.cs_execute)
 		out1 = compiler_wrapper(self.cs_execute, lang)
 		out2 = compiler_wrapper(self.cs_error, lang)
+		self.assertEqual(lang, "C#")
 		self.assertEqual(out1[0], "Hello World\n")
 		self.assertEqual(out1[1], [])
 		self.assertNotEqual(out2[0], "Hello World\n")
-		self.assertEqual(lang, "C#")
 
 	def test_java(self):
 		lang = detect(self.java_execute)
 		out1 = compiler_wrapper(self.java_execute, lang)
 		out2 = compiler_wrapper(self.java_error, lang)
+		self.assertEqual(lang, "Java")
 		self.assertEqual(out1[0], "Hello World\n")
 		self.assertEqual(out1[1], [])
 		self.assertNotEqual(out2[0], "Hello World\n")
-		self.assertEqual(lang, "Java")'''
